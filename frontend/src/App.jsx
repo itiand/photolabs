01fd3681f9404cf6b66a7e138d79eff031258946
Christian Delos Reyes <christian.dreyes@gmail.com>
2023-07-14 16:45:56 -0400

    implement modal, img onClick the modal appears

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import PhotoListItem from './components/PhotoListItem';
 // import PhotoList from './components/PhotoList';
 // import TopicListItem from './components/TopicListItem';
@@ -7,14 +7,20 @@ import React from 'react';
 import topics from './mocks/topics';
 import photos from './mocks/photos';
 import HomeRoute from './components/HomeRoute';
+import PhotoDetailsModal from './components/PhotoDetailsModal';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
 
-const App = () => (
-  <div className="App">
-    <HomeRoute photos={photos} topics={topics}/>
-  </div>
-);
+const App = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <div className="App">
+      <PhotoDetailsModal isModalOpen={isModalOpen} />
+      <HomeRoute photos={photos} topics={topics} setIsModalOpen={setIsModalOpen} />
+    </div>
+  );
+};
 
 export default App;
